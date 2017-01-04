5ebea04c245b8efb86be6d6c2acfafe364348e64
柯禕藍 <yhilan.ko@gmail.com>
2017-01-04 11:33:04 +1100

    fixing windows PouchDB issues

--- system.js
+++ system.js
@@ -8,18 +8,22 @@ $ = require('jquery');
 window.jQuery = require('jquery');
 
 /* PouchDB*/
-const PouchDB = require('pouchdb');
+const PouchDB = require('pouchdb-browser'); // changed from pouchdb to fix a windows issue
 PouchDB.plugin(require('pouchdb-authentication'));
-const localDB = new PouchDB('ailot');
+const localDB = new PouchDB('local');
+console.log(localDB);
+
+/* UUID generation */
+const uuid = require('uuid/v1');
 
 /* app settings */
-let online = false; // change to true to enable online mode
+let online = true; // change to true to enable online mode
 let serverFirst = false; // prioritise remote over local
 let liveSynching = false; // live synchronisation
-let un = "ailot"; // couchdb user
-let pw = ""; // couchdb password
+let un = "ailot"; // admin user for couchdb
+let pw = "E1c59Z5uO09F1pH"; // admin password for couchdb
 var remoteDB = new PouchDB('http://' + un + ':' + pw + '@phonemica.net:5984/ailot');
 
+// curl ailot:E1c59Z5uO09F1pH@localhost:5984
 const appName = "ichō"; // maybe change this at some point
 const altAppName = "銀杏";
-let thisVersion = "000004";
